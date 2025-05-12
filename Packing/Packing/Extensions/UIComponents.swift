3e74f131bff7a9f1c057cb9098aebf485165b514
iyungui <rsdbddml@gmail.com>
2025-05-12 16:13:35 +0900

    [delete] 사용하지 않는 파일제거, ui components 관련 코드 이동

--- Packing/Packing/Extensions/UIComponents.swift
+++ Packing/Packing/Extensions/UIComponents.swift
@@ -130,3 +130,19 @@ extension UILabel {
         attributedText = string
     }
 }
+
+
+extension UIView {
+    @discardableResult
+    func setGradientColor(colorOne: UIColor, colorTwo: UIColor) -> CALayer {
+        let gradientLayer: CAGradientLayer = CAGradientLayer()
+        
+        gradientLayer.frame = self.bounds
+        gradientLayer.colors = [colorOne.cgColor, colorTwo.cgColor]
+        gradientLayer.locations = [0, 1]
+        gradientLayer.startPoint = CGPoint(x: 1.0, y: 1.0)
+        gradientLayer.endPoint = CGPoint(x: 0.0, y: 0.0)
+        layer.insertSublayer(gradientLayer, at: 0)
+        return layer
+    }
+}
